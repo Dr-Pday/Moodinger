ea372ad26fe2f567c62957649935f7f47f3038bf
erfan <erfan.blackeye@gmail.com>
2022-11-24 00:45:30 +0330

    splashscreen background and logo

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
@@ -20,12 +21,26 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              'pattern.png',
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.indigo[900],
+        image: DecorationImage(
+          repeat: ImageRepeat.repeat,
+          image: AssetImage(
+            'assets/images/pattern.png',
+          ),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Container(
+            width: 155,
+            height: 77,
+            child: Image(
+              image: AssetImage(
+                'assets/images/Startlogo.png',
+              ),
             ),
           ),
         ),
